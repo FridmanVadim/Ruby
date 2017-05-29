733f412834866e147f71bdb62d4de0e08c929538
Vadim <fridmanxk3@gmail.com>
2017-05-29 20:52:31 +0500

    Создал объект "кораблик" и "выстрел".

---
+++ Lesson1/DZ1.rb
@@ -0,0 +1,19 @@
+boat = Object.new
+def boat.belonging
+	false
+end
+def boat.number(*n)
+	puts n
+end
+def boat.coordinate(x, y)
+	
+end
+def boat.life?
+	false
+end
+
+
+shot = Object.new
+def shot.coordinate(x, y)
+
+end
